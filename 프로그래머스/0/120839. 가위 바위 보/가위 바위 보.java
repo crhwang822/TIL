f92cc179bffdef8c3ca8882b95ee3e0903269d0d
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-09 08:13:26 +0800

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,21 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<rsp.length(); i++){
+            switch(rsp.charAt(i)){
+                case '2':
+                    sb.append("0");
+                    break;
+                case '0':
+                    sb.append("5");
+                    break;
+                case '5':
+                    sb.append("2");
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+}
