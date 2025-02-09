62fc3e29197b424aca4eecfdfafd4db8ee63cb48
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-09 08:29:11 +0800

    [level 0] Title: 최댓값 만들기 (2), Time: 0.48 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        Arrays.sort(numbers);
+        int a = numbers[0] * numbers[1];
+        int b = numbers[numbers.length-2] * numbers[numbers.length-1];
+        int answer = a>b? a:b;
+        return answer;
+    }
+}
