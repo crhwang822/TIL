09716798941def2e0f2d5a035ee0308dc1713bd2
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-09 19:50:11 +0800

    [level 0] Title: 모스부호 (1), Time: 0.20 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,42 +1,16 @@
 import java.lang.StringBuilder;
-import java.util.HashMap;
+import java.util.List;
 
 class Solution {
     public String solution(String letter) {
-        //모스 부호 맵
-        HashMap<String,String> morseMap = new HashMap<>();
-        morseMap.put(".-", "a");
-        morseMap.put("-...", "b");
-        morseMap.put("-.-.", "c");
-        morseMap.put("-..", "d");
-        morseMap.put(".", "e");
-        morseMap.put("..-.", "f");
-        morseMap.put("--.", "g");
-        morseMap.put("....", "h");
-        morseMap.put("..", "i");
-        morseMap.put(".---", "j");
-        morseMap.put("-.-", "k");
-        morseMap.put(".-..", "l");
-        morseMap.put("--", "m");
-        morseMap.put("-.", "n");
-        morseMap.put("---", "o");
-        morseMap.put(".--.", "p");
-        morseMap.put("--.-", "q");
-        morseMap.put(".-.", "r");
-        morseMap.put("...", "s");
-        morseMap.put("-", "t");
-        morseMap.put("..-", "u");
-        morseMap.put("...-", "v");
-        morseMap.put(".--", "w");
-        morseMap.put("-..-", "x");
-        morseMap.put("-.--", "y");
-        morseMap.put("--..", "z");
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        List<String> morseList = List.of(morse);
         
         //매핑
         StringBuilder sb = new StringBuilder();
         String[] strlist = letter.split(" ");
         for(String str:strlist)
-            sb.append(morseMap.get(str));
+            sb.append((char)(morseList.indexOf(str) + (int)'a'));
         
         return sb.toString();
     }
