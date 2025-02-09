d49eef72250bf0817bb65718e15bfd1129f20c71
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-09 08:59:42 +0800

    [level 0] Title: 약수 구하기, Time: 0.27 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -8,7 +8,11 @@ class Solution {
             if(n%i==0)
                 list.add(i);
         }
-        int[] answer = list.stream().mapToInt(i->i).toArray();
+        
+        int[] answer = new int[list.size()];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = list.get(i);
+        }
         return answer;
     }
 }
