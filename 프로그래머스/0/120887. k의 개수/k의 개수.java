c73791fde68359d4145aca1730c63e156e42f1b1
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-09 20:40:35 +0800

    [level 0] Title: k의 개수, Time: 48.07 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
         for(int num = i; num<j+1; num++){
-            for(int index = 0; index<String.valueOf(num).length(); index++){
-                char c = String.valueOf(num).charAt(index);
-                if (String.valueOf(c).equals(String.valueOf(k)))
+            String str = num + "";
+            for(int index = 0; index<str.length(); index++){
+                if (str.charAt(index) - '0' == k)
                     answer++;
             }
         }
