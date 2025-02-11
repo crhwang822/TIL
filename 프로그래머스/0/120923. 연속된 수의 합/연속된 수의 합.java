de50d739d6e5d567f7c8dc73713d6180bdcf1396
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-12 04:35:24 +0800

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,15 +1,11 @@
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
-        int start = 0;
         
-        if(num%2==0){
-            start = (total - (num*num)/2 + num/2) / num;
-
-        }
-        else{
-            start = total/num - num/2;
-        }
+        //연속합은 num * x + (0 + 1 + ... + (n-1)) 로 표현된다.
+        //상수항은 등차수열의 합 공식을 사용해 구하기
+        int c = (num * (num - 1)) / 2;
+        int start = (total - c) / num;
         
         for(int i=0; i<num; i++)
             answer[i] = start + i;
