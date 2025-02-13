fa3868af939c33a35e822e235fe49bb13b06516c
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-14 01:58:45 +0800

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.11 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <= right; i++){
+            if(Math.sqrt(i) % 1 == 0){
+                answer -= i;
+                continue;
+            }
+            answer += i;
+        }
+        return answer;
+    }
+}
