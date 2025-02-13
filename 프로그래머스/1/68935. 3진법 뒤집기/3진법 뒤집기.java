4768307f009d91bf8a456551cce00a8392c02d97
Chaerin Hwang <87927105+crHwang0822@users.noreply.github.com>
2025-02-14 07:41:08 +0800

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,11 @@
+import java.lang.StringBuilder;
+
+class Solution {
+    public int solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Integer.toString(n,3));
+        sb.reverse();
+        int answer = Integer.parseInt(sb.toString(),3);
+        return answer;
+    }
+}
