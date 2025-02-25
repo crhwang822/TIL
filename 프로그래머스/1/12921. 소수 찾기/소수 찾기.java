b714efe5f855b9ad26e72fe9a7d05421b078e576
Chaerin Hwang <crhwang822@gmail.com>
2025-02-25 18:52:25 +0900

    [level 1] Title: 소수 찾기, Time: 202.44 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 1;  //2는 무조건 포함
+        
+        for(int i = 2; i <= n; i++){
+            if(i % 2 == 0) //2를 제외한 짝수는 포함 X
+                continue;
+            answer += isDecimal(i);
+        }
+        
+        return answer;
+    }
+    
+    public int isDecimal(int n){
+        for(int i = 3; i <= Math.sqrt(n); i++){
+            if(n % i == 0)
+                return 0;
+        }
+        return 1;
+    }
+}
