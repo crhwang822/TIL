4fa2b3b66fdb1a2c280dcdfca901eff8a95c3a98
Chaerin Hwang <crhwang822@gmail.com>
2025-03-01 20:25:33 +0900

    [level 1] Title: 체육복, Time: 2.58 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        int answer = n - lost.length;
+        
+        //잃어버린 학생과 여벌을 가져온 학생이 일치하는 경우
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if(lost[i] == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        for(int i = 0; i < lost.length; i++){
+            for(int j = 0; j < reserve.length; j++){
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]){
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
