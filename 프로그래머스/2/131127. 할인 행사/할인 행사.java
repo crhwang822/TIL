f28c7e3e6af8a28783cf342333fa36662b237b88
Chaerin Hwang <crhwang822@gmail.com>
2025-04-06 20:58:40 +0900

    [level 2] Title: 할인 행사, Time: 53.64 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,29 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        List<String> wish = Arrays.asList(want);
+        List<String> wish = new ArrayList<>();
+        
+        for(int i = 0; i < want.length; i++) {
+            for(int j = 0; j < number[i]; j++)
+                wish.add(want[i]);
+        }
         
         for(int i = 0; i < discount.length - 9; i++) {
             String[] items = Arrays.copyOfRange(discount, i, i + 10);
-            if(isIncluded(items, wish, number.clone()))
+            if(isIncluded(items, new ArrayList<>(wish)))
                 answer++;
         }
         
         return answer;
     }
     
-    private boolean isIncluded(String[] items, List<String> wish, int[] number) {
+    private boolean isIncluded(String[] items, List<String> wish) {
         for(String item : items) {
             if(wish.contains(item)) {
-                number[wish.indexOf(item)] -= 1;
+                wish.remove(item);
             }
         }
+
+        if(wish.isEmpty())
+            return true;
         
-        for(int i = 0; i < number.length; i++)
-            if(number[i] > 0)
-                return false;
-        
-        return true;
+        return false;
     }
     
 }
