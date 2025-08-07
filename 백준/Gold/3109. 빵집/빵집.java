2b0a3d3a2bdddc1ed3a344e94d0fcadaf4c22539
Chaerin Hwang <crhwang822@gmail.com>
2025-08-07 16:09:18 +0900

    [Gold II] Title: 빵집, Time: 1548 ms, Memory: 332396 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[r][c];
+
+        for(int i = 0; i < r; i++) {
+            String[] arr = br.readLine().split("");
+            for(int j = 0; j < c; j++) {
+                String s = arr[j];
+                if(s.equals("x")) {
+                    map[i][j] = -1;
+                } else {
+                    map[i][j] = 0;
+                }
+            }
+        }
+
+        int answer = 0;
+        boolean[][] visited = new boolean[r][c];
+        for(int i = 0; i < r; i++) {
+            if(dfs(map, visited, i, 0, i + 1))
+                answer++;
+        }
+
+
+        bw.write(answer + "");
+        bw.close();
+    }
+
+    public static boolean dfs(int[][] map, boolean[][] visited,int r, int c, int n) {
+        if(c == map[0].length - 1)
+            return true;
+
+        if(map[r][c] != 0)
+            return false;
+        map[r][c] = n;
+
+        if(r > 0 && !visited[r-1][c] && dfs(map, visited, r - 1, c + 1, n))
+            return true;
+
+        if(!visited[r][c] && dfs(map, visited, r, c + 1, n))
+            return true;
+
+        if(r < map.length - 1 && !visited[r+1][c] &&dfs(map, visited, r + 1, c + 1, n))
+            return true;
+
+        map[r][c] = 0;
+        visited[r][c] = true;
+        return false;
+    }
+
+}
