b7e87dd6b697b326240b4768004d9ea4520269c3
Chaerin Hwang <crhwang822@gmail.com>
2025-09-16 12:22:51 +0900

    [Silver II] Title: 이항 계수 2, Time: 108 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+
+    public static int mod = 10007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        long[] fact = new long[n + 1];
+        fact[0] = 1;
+        fact[1] = 1;
+        for(int i = 2; i <= n; i++)
+            fact[i] = (fact[i - 1] * i) % mod;
+
+        long answer = 0;
+        answer = ((fact[n] % mod) * (pow(fact[k], mod - 2) % mod)) % mod;
+        answer = answer * (pow(fact[n - k], mod - 2) % mod);
+        answer %= mod;
+
+        bw.write(answer + "");
+        bw.close();
+    }
+
+    public static long pow (long a, long b) {
+        long ans = 1;
+        long base = a;
+        while(b > 0) {
+            if(b % 2 == 1){
+                ans = (ans * base) % mod;
+                b--;
+            }
+            else {
+                base = (base * base) % mod;
+                b /= 2;
+            }
+        }
+        return ans;
+    }
+
+}
