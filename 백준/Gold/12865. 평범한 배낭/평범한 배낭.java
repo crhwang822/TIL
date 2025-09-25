f9b1f6a28f306cf1edc0da2ed7bf9888fc718e51
Chaerin Hwang <crhwang822@gmail.com>
2025-09-25 10:13:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 53964 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,38 +1,36 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+        int[][] obj = new int[n][2];
+        int[][] dp = new int[n + 1][k + 1];
 
-        int[][] items = new int[n][2];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            items[i][0] = Integer.parseInt(st.nextToken());
-            items[i][1] = Integer.parseInt(st.nextToken());
+            obj[i][0] = Integer.parseInt(st.nextToken());
+            obj[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        int[][] dp = new int[k+1][n+1];
         for(int i = 1; i < dp.length; i++) {
-            for(int j = 1; j < dp[0].length; j++) {
-                int weight = items[j-1][0];
-                dp[i][j] = dp[i][j-1];
-                if(i >= weight) {
-                    dp[i][j] = Math.max(dp[i][j], dp[i-weight][j-1] + items[j-1][1]);
-                }
+            for(int w = 0; w < dp[0].length; w++) {
+                dp[i][w] = dp[i - 1][w];
+                if(w >= obj[i - 1][0])
+                    dp[i][w] = Math.max(dp[i][w], dp[i - 1][w - obj[i - 1][0]] + obj[i - 1][1]);
             }
         }
 
-        int max = 0;
-        for(int i = 1; i < dp[0].length; i++) {
-            max = Math.max(max, dp[dp.length - 1][i]);
-        }
-
-        bw.write(max+"");
+        sb.append(dp[n][k]);
+        bw.write(sb.toString());
         bw.close();
     }
+
 }
