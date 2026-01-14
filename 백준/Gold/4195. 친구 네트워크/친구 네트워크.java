fd8eb3d5cb25a00a19652f3a4eff98c4cc37e923
Chaerin Hwang <crhwang822@gmail.com>
2026-01-14 13:41:52 +0900

    [Gold II] Title: 친구 네트워크, Time: 784 ms, Memory: 72724 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -1,55 +1,64 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+        import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+
+    static Map<String, String> map;
+
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
         int T = Integer.parseInt(br.readLine());
+        map = new HashMap<>();
+        Map<String, Integer> countMap = new HashMap<>();
 
-        for(int Ti = 0; Ti < T; Ti++) {
-            int n = Integer.parseInt(br.readLine());
-            Map<String, String> map = new HashMap<>();
-            Map<String, Integer> sizeMap = new HashMap<>();
+        for(int testCase = 0; testCase < T; testCase++) {
+            int F = Integer.parseInt(br.readLine()); // <= 100,000
+            String name1, name2;
+            String start = "";
+            map.clear();
+            countMap.clear();
 
-            for(int i = 0; i < n; i++) {
-                String[] arr = br.readLine().split(" ");
-                String name = arr[0];
-                String other = arr[1];
+            for(int i = 0; i < F; i++) {  //100,000
+                st = new StringTokenizer(br.readLine());
+                name1 = st.nextToken();
+                name2 = st.nextToken();
 
-
-                if(!map.containsKey(name)) {
-                    map.put(name, name);
-                    sizeMap.put(name, 1);
+                if(i == 0) {
+                    start = name1;
                 }
 
-                if(!map.containsKey(other)) {
-                    map.put(other, other);
-                    sizeMap.put(other, 1);
+                if(!map.containsKey(name1)) {
+                    map.put(name1, name1);
+                    countMap.put(name1, 1);
                 }
 
-                String nameParent = find(map, name);
-                String otherParent = find(map, other);
-
-                if(!nameParent.equals(otherParent)) {
-                    map.put(otherParent, nameParent);
-                    sizeMap.replace(nameParent, sizeMap.get(nameParent) + sizeMap.get(otherParent));
+                if(!map.containsKey(name2)) {
+                    map.put(name2, name2);
+                    countMap.put(name2, 1);
                 }
 
-                bw.write(sizeMap.get(nameParent) + "\n");
+                if(!find(name1).equals(find(name2))) {
+                    int cnt = countMap.get(find(name1)) + countMap.get(find(name2));
+                    countMap.replace(find(name1), cnt);
+                    countMap.replace(find(name2), cnt);
+                    map.replace(find(name2), find(name1));
+                }
+
+                bw.write(countMap.get(find(name1)) + "\n");
             }
+
         }
 
         bw.close();
     }
 
-    public static String find(Map<String, String> map, String s) {
-        if(!map.get(s).equals(s)) {
-            map.put(s, find(map, map.get(s))); // 경로 압축
+    public static String find(String name) {
+        if(!map.get(name).equals(name)) {
+            map.replace(name, find(map.get(name)));
         }
-        return map.get(s);
+        return map.get(name);
     }
 }
